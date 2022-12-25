fd5c8545de083276284818f4d89b8774d22fd250
vsm <devops.july2.2017@gmail.com>
2022-12-25 22:04:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 helm repo add elastic https://helm.elastic.co
 helm repo update
-helm install elk-elasticsearch elastic/elasticsearch -f values-2.yaml --namespace logging --create-namespace
+helm install elk-elasticsearch elastic/elasticsearch -f values2-es.yaml --namespace logging --create-namespace
+
+kibana:
+helm install elk-kibana elastic/kibana -f values-2.yamls
